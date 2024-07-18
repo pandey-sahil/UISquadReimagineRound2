766eba7bc453c808cf4db9af9a229f3ca171a09b
Vivek ghoshi <the.vivekghoshi@gmail.com>
2024-07-18 22:59:07 +0530

    footer

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// #scrolltrigger locomotive code ??
+function page5Animation(){
+    document.addEventListener("DOMContentLoaded", () => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.fromTo("#container", 
+            {
+                opacity: 0, 
+                y: 50
+            }, 
+            {
+                opacity: 1,
+                y: 0,
+                duration: 1,
+                ease: "power3.out",
+                scrollTrigger: {
+                    trigger: "#container",
+                    start: "top 80%",
+                    toggleActions: "play none none none"
+                }
+            }
+        );
+    });
+}
+page5Animation();
